50745dff7d0552f341c4f09f9f2bad263c5c4446
Egor Mikhaylov <ber4bbit@gmail.com>
2022-09-26 23:25:59 +0700

    added live validation and check for all inputs before submition

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-const exampleFunction = () => {
-    console.log('hello');
+const form = document.forms['register-form'];
+const inputs = Array.from(document.querySelectorAll('input'));
+const submitBtn = document.getElementById('submitBtn');
+
+inputs.map(input => input.getAttribute('data-is-valid')).includes('0') ? submitBtn.classList.add('disabled') : submitBtn.classList.remove('disabled');
+
+const validateInput = (event) => {
+    const inputValue = event.target.value;
+    const inputRegExp = new RegExp(event.target.getAttribute('data-regexp'));
+
+    inputRegExp.test(inputValue) && inputValue != ''
+        ? 
+        (event.target.style.border = 'solid 1px green', event.target.setAttribute('data-is-valid', 1)) 
+        : 
+        (event.target.style.border = 'solid 1px red', event.target.setAttribute('data-is-valid', 0));
+
+    inputs.map(input => input.getAttribute('data-is-valid')).includes('0') ? submitBtn.classList.add('disabled') : submitBtn.classList.remove('disabled');
 }
+
+const btnHandler = () => {
+    inputs.forEach(input => {
+        console.log(input.value);
+    })
+}
+
+form.addEventListener('input', validateInput);
+submitBtn.addEventListener('click', btnHandler);
