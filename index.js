fb56b53e74256973f03c56bec61f5f2a5bed1d0d
Egor Mikhaylov <ber4bbit@gmail.com>
2022-09-28 15:52:33 +0700

    added match password check

--- index.js
+++ index.js
@@ -6,14 +6,22 @@ inputs.map(input => input.getAttribute('data-is-valid')).includes('0') ? submitB
 
 const validateInput = (event) => {
     const inputValue = event.target.value;
+    const inputName = event.target.getAttribute('name');
     const inputRegExp = new RegExp(event.target.getAttribute('data-regexp'));
 
-    inputRegExp.test(inputValue) && inputValue != ''
+    if (inputName === 'user-repassword') {
+        inputValue === inputs[2].value && inputValue != ''
+            ?
+            (event.target.style.border = 'solid 1px green', event.target.setAttribute('data-is-valid', 1))
+            :
+            (event.target.style.border = 'solid 1px red', event.target.setAttribute('data-is-valid', 0)) 
+    } else {
+        inputRegExp.test(inputValue) && inputValue != ''
         ? 
         (event.target.style.border = 'solid 1px green', event.target.setAttribute('data-is-valid', 1)) 
         : 
         (event.target.style.border = 'solid 1px red', event.target.setAttribute('data-is-valid', 0));
-
+    }
     inputs.map(input => input.getAttribute('data-is-valid')).includes('0') ? submitBtn.classList.add('disabled') : submitBtn.classList.remove('disabled');
 }
 
